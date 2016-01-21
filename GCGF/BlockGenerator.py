a438c6608433b88905e650c5fff734480ec52294
Michael Gonzalez <mmg005@ucsd.edu>
2016-01-21 09:47:02 -0800

    Added better commenting to BlockGenerator

--- GCGF/BlockGenerator.py
+++ GCGF/BlockGenerator.py
@@ -8,31 +8,12 @@ import json
 import jinja2
 from clangTest import *
 
-
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
-
-def createBlockName( func ):
-    name = func.name
-    name[0].title()
-    i = 0
-    for param in func.params:
-        if( i is 0):
-	    name = name + " with "
-	else:
-	    name = name + " and "
-        name = name + param[0] + " as "
-	name = name + " %'" + param[0] + "'"
-	i = i + 1
-    return name
-def determineBlockType( func ):
-    type = func.returnType.lower()
-    if( type == "int" or type == "float" or type == "double"):
-        return "reporter"
-    if( type == "bool" or type == "boolean" ):
-        return "predicate"
-    else:
-        return "command"
+# Function Name: determineParamType()
+# Arguments: param - A string representation of a function's parameter
+# Returns: The string which blockly uses to constrain block input types
+# Error Handling: If the parameter passed to this function does not match any
+#                 of the handled types, then this function will raise an 
+#                 exception. THIS FUNCTION IS NOT EXHAUSTIVE
 def determineParamType( param ):
     cleaned = param.lower()
     if( cleaned == "int" or cleaned == "float" or cleaned == "double"):
@@ -44,72 +25,75 @@ def determineParamType( param ):
     else:
         print "FunctionParam Error: Could not determine parameter type in determineParamType in BlockGenerator.py"
 	raise
-# Arg parse stuff
-parser = argparse.ArgumentParser(description="Generates XML objects that represent each function in our arduino libraries")
+# Setup argument parsing
+parser = argparse.ArgumentParser(description="BlockGenerator.py creates a Blockly IDE for Gadgetron. It parses our existing C++ clase libraries using Clang to generate block categories and blocks. It then uses Jinja to actually create the IDE")
 parser.add_argument("-l", "--library", required=True)
 parser.add_argument("-j", "--jinja", required=True)
 args = parser.parse_args()
-# Xml stuff
-libxml = ET.parse( args.library )
-library = libxml.getroot()
+
+
 # Prep Jinja Template
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 template = JINJA_ENVIRONMENT.get_template(args.jinja)
-# Clang stuff
-clang.cindex.Config.set_library_path('/usr/lib/x86_64-linux-gnu')
-if(platform.platform().split('-')[0].lower() == "darwin"):
-    clang.cindex.Config.set_library_path('/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib')
-index = clang.cindex.Index.create()
-# lxml generator stuff
+# This variable is used later in the script to pass variables to the template
+jinja_vars = {"blocklist":[]}
+
+# Prepare lxml 
+# This script uses lxml to build the Blockly toolbox
 root = ET2.Element("xml")
 root.set("id", "toolbox")
 root.set("style", "display: none")
-def printClassFunctions( aClass ):
-    for aFunction in aClass.functions:
-    #print aFunction
-        hasSetup = False
-        newBlock = ET.SubElement( root, "block-definition" )
-        newBlock.set("s", createBlockName(aFunction) )
-        newBlock.set("type", determineBlockType(aFunction))
-        ET.SubElement( newBlock, "header" )
-        ET.SubElement( newBlock, "code" )
-        inputs = ET.SubElement( newBlock, "inputs" )
-        for param in aFunction.params:
-            input = ET.SubElement( inputs, "input" )
-	    input.set("type", determineParamType( param[1] ))
-	    if( param[0] is "setup" ):
-	        hasSetup = True
-	return hasSetup
-jinja_vars = {"blocklist":[]}
+
+
+# Prepare Clang
+# This step has only been tested on the iMac in the NVSL and Michael's
+# laptop. 
+# This line should be the path of the clang library on Ubuntu
+clang.cindex.Config.set_library_path('/usr/lib/x86_64-linux-gnu')
+# This line should be the path of the clang library on Darwin 
+if(platform.platform().split('-')[0].lower() == "darwin"):
+    clang.cindex.Config.set_library_path('/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib')
+index = clang.cindex.Index.create()
+
+# The library argument passed to this script contains the locations of each of
+# the C++ class files. This grabs the root of that xml file
+libxml = ET.parse( args.library )
+library = libxml.getroot()
+# Then iterate over each component in library
 for component in library:
+    # Grab the real class file's location
     path = component.attrib["path"]
     path = os.path.expandvars( path )
-    #print path
+    # Create a clang translation unit to traverse the class
     translation_unit = index.parse(path, ['-x', 'c++', '-std=c++11', '-D__CODE_GENERATOR__'])
     classes = build_classes(translation_unit.cursor, path)
+
     # Iterate over all classes
     for aClass in classes:
-        # Pick a random color to represent this category
+        # Pick a random color to represent this class's category
         currColor = random.randrange(0,360)
-	# Start the xml definition for this category
+	# Start the xml definition for this category's toolbox representation
         newBlock = ET.SubElement(root, "category") 
-	# Set the name attribute
+	# Set the category's name attribute
 	newBlock.set("name", aClass.name)
-	# Set the colour attribute
+	# Set the category's colour attribute
 	newBlock.set("colour", str(currColor))
-	# Give each function a unique number
-	fp = 0
 	# Iterate over all the functions in the current class
+	fp = 0
 	for func in aClass.functions:
-	    # Create the standard json definition
+	    # Create a blockly json definition
+	    # The arrtibutes passed here should be the same accross all blockly json files
 	    funcjson = {
 	            "id":"_"+aClass.name+"_"+func.name + str(fp),
 	            "message0":aClass.name+" "+func.name + " %1",
                     "colour" : currColor,
 	            "tooltip" : "",
 	            "helpUrl": "gadgetron.build" }
-            fp += 1
             args = [{"type":"input_dummy"}]
-	    i = 2 # We want to keep an index to update our message0 attribute later
+	    # We want to keep an index to update our message0 attribute later
+	    i = 2
+            fp += 1
 	    # Iterate over all the arguments for the current function
 	    for arg in func.params:
 	        argJson = {"type":"input_value",
