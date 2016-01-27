01745a4f76ed04f07e3a61a8442cbfef4b358dc9
Michael Gonzalez <mmg005@ucsd.edu>
2016-01-27 15:05:57 -0800

    Added default arduino functions

--- Blockly/custom_blocks.js
+++ Blockly/custom_blocks.js
@@ -14,6 +14,31 @@ Blockly.Blocks['main_loop'] = {
         .appendField("Put your repeating code here");
     this.setNextStatement(true);
     this.setTooltip('');
-    this.setHelpUrl('http://www.example.com/');
+    this.setHelpUrl('http://www.gadgetron.build/');
+  }
+};
+
+Blockly.Blocks['delay'] = {
+  init: function() {
+    this.appendValueInput("NAME")
+        .appendField("Pause for ");
+    this.appendDummyInput()
+        .appendField("milliseconds");
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(210);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.gadgetron.build/');
+  }
+};
+
+Blockly.Blocks['millis'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Time in milliseconds");
+    this.setOutput(true);
+    this.setColour(210);
+    this.setTooltip('The length of time since the program has started in milliseconds');
+    this.setHelpUrl('http://www.gadgetron.build/');
   }
 };
