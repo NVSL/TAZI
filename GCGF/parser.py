51ae3641a3eb98ca09c9a30ce78f878f6fcad26c
Priyanka Kulshreshtha <pinkikul@yahoo.com>
2016-02-04 22:34:30 -0800

    Parser actually parses both files now

--- GCGF/parser.py
+++ GCGF/parser.py
@@ -1,6 +1,23 @@
 import xml.etree.ElementTree
-e = xml.etree.ElementTree.parse('gspec.xml').getroot()
+gspec = xml.etree.ElementTree.parse('gspec.xml').getroot()
+base = xml.etree.ElementTree.parse('Components.cat').getroot()
 
-for node in e:
+compList = []
+finalList = []
+
+#Parsing the components file from Gadgetron
+for component in base.findall('component'):
+    compStr = component.get('keyname');
+    for child in component:
+        if child.tag == 'API':
+	    compList.append(compStr)
+	    print 'API FOUND FOR ABOVE ELEMENT ', compStr
+
+
+#Parsing the gspec (made in Jet)
+for node in gspec:
     if node.tag == "component":
-        print node.attrib["type"]
+        attribStr = node.attrib["type"]
+        if attribStr in compList:
+             finalList.append(attribStr)
+	     print 'FOUND IN BOTH ', node.attrib["type"]
