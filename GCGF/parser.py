1b77dbfd453f971d216a240b1a7f97d54984e870
Priyanka Kulshreshtha <pinkikul@yahoo.com>
2016-02-06 21:42:06 -0800

    fixed stuff

--- GCGF/parser.py
+++ GCGF/parser.py
@@ -1,23 +1,37 @@
 import xml.etree.ElementTree
-gspec = xml.etree.ElementTree.parse('gspec.xml').getroot()
-base = xml.etree.ElementTree.parse('Components.cat').getroot()
 
-compList = []
-finalList = []
+class gspecParse: 
 
-#Parsing the components file from Gadgetron
-for component in base.findall('component'):
-    compStr = component.get('keyname');
-    for child in component:
-        if child.tag == 'API':
-	    compList.append(compStr)
-	    print 'API FOUND FOR ABOVE ELEMENT ', compStr
+   def initializeList(self):
+      base = xml.etree.ElementTree.parse('Components.cat').getroot()
+      compList = []
+      #Parsing the components file from Gadgetron
+      for component in base.findall('component'):
+         compStr = component.get('keyname');
+         for child in component:
+            if child.tag == 'API':
+	       compList.append(compStr)
+	       print 'API FOUND FOR ABOVE ELEMENT ', compStr
+      return compList
 
+   def getComps(self, compFile, compList): 
+      gspec = xml.etree.ElementTree.parse(compFile).getroot()
+      finallist =[]
+      finalDict = dict()
+      #Parsing the gspec (made in Jet)
+      for node in gspec:
+         if node.tag == "component":
+            attribStr = node.attrib["type"]
+            if attribStr in compList:
+	       if attribStr in finalDict:
+	          finalDict[attribStr] = finalDict[attribStr] + 1
+		  print "Increased count ", attribStr
+	       else:
+	          finalDict[attribStr] = 0
+	          print "ADDED ", attribStr
+               finallist.append(attribStr)
+	       print 'FOUND IN BOTH ', node.attrib["type"]
 
-#Parsing the gspec (made in Jet)
-for node in gspec:
-    if node.tag == "component":
-        attribStr = node.attrib["type"]
-        if attribStr in compList:
-             finalList.append(attribStr)
-	     print 'FOUND IN BOTH ', node.attrib["type"]
+    
+gp = gspecParse()
+gp.getComps("gspec.xml", gp.initializeList())  
