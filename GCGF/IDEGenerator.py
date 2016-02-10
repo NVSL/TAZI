47ee242cf2af9012f2f085eedb7367af0d56c335
MichaelMGonzalez <mmg005@ucsd.edu>
2016-02-09 22:13:44 -0800

    checks if passed json is file

--- GCGF/IDEGenerator.py
+++ GCGF/IDEGenerator.py
@@ -1,5 +1,6 @@
 import json
 import GspecParser
+import os
 from StringIO import StringIO
 
 
@@ -8,7 +9,12 @@ class IDEGenerator:
     def loadBlockDefinitions( self, blockJson ):
         # Type checking
         if type (blockJson) == str:
-            stream = StringIO( blockJson )
+            stream = None
+            # Is it a file?
+            if os.path.isfile( blockJson):
+                stream = open(blockJson)
+            else:
+                stream = StringIO( blockJson )
             self.blocks = json.load(stream)
         # For some reason, it seems like json objects get passed as lists
         elif type (blockJson) == list:
@@ -16,11 +22,12 @@ class IDEGenerator:
         else:
             print "Unknown type: ", type(blockJson)
             raise "Unknown type passed to loadBlockDefinitions"
-
+        print json.dumps(self.blocks)
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="IDEGenerator.py creates a Blockly IDE for Gadgetron. It uses Jinja to create the IDE")
     parser.add_argument("-j", "--json", required=True)
+    args = parser.parse_args()
     generator = IDEGenerator()
-    generator.loadBlockDefinitions( '["streaming API"]' )
+    generator.loadBlockDefinitions( args.json )
