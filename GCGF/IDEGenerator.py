5db99534e26f273885512bdb0f553fdf914f7809
MichaelMGonzalez <mmg005@ucsd.edu>
2016-02-09 23:54:58 -0800

    finished first IDEGenerator draft

--- GCGF/IDEGenerator.py
+++ GCGF/IDEGenerator.py
@@ -1,4 +1,5 @@
 import json
+import xml.etree.ElementTree as ET
 from GspecParser import GspecParser
 import os
 import copy
@@ -16,7 +17,7 @@ class IDEGenerator:
         self.jinja_vars = {"blocklist":[]}
         
     def setJinjaTemplate(self, template):
-        self.JINJA_ENVIRONMENT.get_template(template)
+        self.template = self.JINJA_ENVIRONMENT.get_template(template)
         
     # Loads default blocks xml to build the Blockly toolbox
     def loadDefaultBlocks( self, blocksXml ):
@@ -56,15 +57,44 @@ class IDEGenerator:
         
     def createBlockSubset(self):
         self.blockCategories = {}
+        totalNumberOfCategories = 0
+        componentIdx = 1.0
+        for k in self.components.keys():
+            totalNumberOfCategories = totalNumberOfCategories + self.components[k]
         for component in self.components.keys():
             for i in range( 1, self.components[component] + 1 ):
+                # Some useful aliases
+                name = component + " " + str(i)
+                
+                # We want a new colour for this subset
+                color = str(int((componentIdx / totalNumberOfCategories) * 360))
+                componentIdx = componentIdx + 1
+                
                 # The blocks for just this component will be called localBlocks
                 localBlocks = {}
+                
+                # Create the categoriesXML node
+                categoryNode = ET.SubElement(self.categoriesXML, "category" )
+                categoryNode.attrib["name"] = name
+                categoryNode.attrib["colour"] = color
+                
+                # Iterate over each block in our copy so we can make a unique instance
                 jsonElem = copy.deepcopy(self.blocks[component])
                 for block in jsonElem:
                     block["id"] = "_" + component.lower() + str(i) + "_" + block["id"]
+                    id = block["id"]
+                    block["colour"] = color
+                    # Add the new block to its proper category
+                    blockNode = ET.SubElement(categoryNode, "block")
+                    blockNode.attrib["type"] = id
+                    blockNode.text = " "
                     localBlocks[ block["id"] ] = block
-                self.blockCategories[ component + " " + str(i) ] = localBlocks
+                    self.jinja_vars["blocklist"].append( [ id, str(json.dumps(block)) ] )
+                self.blockCategories[ name ] = localBlocks
+        self.jinja_vars["toolbox"] = str(ET.tostring( self.categoriesXML ))
+    
+    def renderIDE(self):
+        return self.template.render(self.jinja_vars)
                 
 if __name__ == "__main__":
     import argparse
@@ -72,10 +102,16 @@ if __name__ == "__main__":
     parser.add_argument("-j", "--json", required=True)
     parser.add_argument("-c", "--catalog", required=True)
     parser.add_argument("-g", "--gspec", required=True)
+    parser.add_argument("-d", "--default_blocks", required=True)
+    parser.add_argument("-x", "--jinja", required=True)
     args = parser.parse_args()
     generator = IDEGenerator()
+    generator.setJinjaTemplate( args.jinja )
     generator.loadBlockDefinitions( args.json )
+    generator.loadDefaultBlocks( args.default_blocks )
     generator.loadGspec( args.gspec, args.catalog )
     generator.createBlockSubset()
-    for key in generator.blockCategories.keys():
-        print key, json.dumps(generator.blockCategories[key], indent=4)
+    #for key in generator.blockCategories.keys():
+    #    print key, json.dumps(generator.blockCategories[key], indent=4)
+    #print ET.tostring(generator.categoriesXML )
+    print generator.renderIDE()
