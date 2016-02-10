23bb5f6f20ae202b9468e97fdd0498994df360ad
MichaelMGonzalez <mmg005@ucsd.edu>
2016-02-09 22:03:06 -0800

    minor edits

--- GCGF/IDEGenerator.py
+++ GCGF/IDEGenerator.py
@@ -9,10 +9,10 @@ class IDEGenerator:
         # Type checking
         if type (blockJson) == str:
             stream = StringIO( blockJson )
-            jsonObj = json.load(stream)
+            self.blocks = json.load(stream)
         # For some reason, it seems like json objects get passed as lists
         elif type (blockJson) == list:
-            jsonObj = blockJson
+            self.blocks = blockJson
         else:
             print "Unknown type: ", type(blockJson)
             raise "Unknown type passed to loadBlockDefinitions"
@@ -23,6 +23,4 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="IDEGenerator.py creates a Blockly IDE for Gadgetron. It uses Jinja to create the IDE")
     parser.add_argument("-j", "--json", required=True)
     generator = IDEGenerator()
-    stream = StringIO( '["streaming API"]' )
-    jsonObj = json.load(stream)
-    generator.loadBlockDefinitions( jsonObj )
+    generator.loadBlockDefinitions( '["streaming API"]' )
