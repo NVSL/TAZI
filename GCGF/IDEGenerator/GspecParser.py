89102f8ecde3edf550b8cf7eb2148eedbae6e83b
Michael Gonzalez <mmg005@ucsd.edu>
2016-02-10 17:07:24 -0800

    Made TAZI more compatible with jet

---
+++ GCGF/IDEGenerator/GspecParser.py
@@ -0,0 +1,69 @@
+import xml.etree.ElementTree as ET
+import os
+
+class GspecParser: 
+
+   def __init__(self):
+       self.catalogTree = None
+   def setCatalog(self, catalog):
+       if type( catalog ) is str:
+           if os.path.isfile( catalog ):
+               self.catalogTree = ET.parse(catalog).getroot()
+           else:
+               self.catalogTree = ET.fromstring( catalog ).getroot()
+   def initializeList(self):
+      if self.catalogTree is None:
+          raise BaseException("Catalog root was never set!")
+      base = self.catalogTree
+      
+      componentsDict = {}
+      #Parsing the components file from Gadgetron
+      for component in base.findall('component'):
+         compStr = component.get('keyname')
+         instanceName = None
+         for child in component:
+         # Check if the component has an API tag
+            if child.tag == 'API':
+	        for node in child:
+		    if node.tag.lower() == 'arduino':
+		        for n in node:
+			    if n.tag.lower() == 'class':
+	                        componentsDict[compStr] = n.attrib["name"]
+	                        #print 'API FOUND FOR ABOVE ELEMENT ', n.attrib["name"]
+      return componentsDict
+
+   def getComps(self, compFile):
+      componentsDict = self.initializeList()
+      gspec = None
+      if type( compFile) is str:
+          if os.path.isfile( compFile):
+              gspec = ET.parse(compFile).getroot()
+          else:
+              gspec = ET.fromstring(compFile).getroot()
+      elif type( compFile ) is unicode:
+          gspec = ET.fromstring(compFile)
+      finallist =[]
+      finalDict = dict()
+      #Parsing the gspec (made in Jet)
+      for node in gspec:
+         if node.tag == "component":
+            attribStr = node.attrib["type"]
+            if attribStr in componentsDict.keys():
+                instanceName = componentsDict[ attribStr]
+                if instanceName in finalDict:
+                    finalDict[instanceName] = finalDict[instanceName] + 1
+                    #print "Increased count ", attribStr
+                else:
+	                finalDict[instanceName] = 1
+	                #print "ADDED ", attribStr
+	                finallist.append(attribStr)
+                #print 'FOUND IN BOTH ', instanceName 
+      return finalDict
+
+    
+if __name__ == "__main__":
+    gp = GspecParser()
+    gp.setCatalog( "Components.cat" )
+    dict = gp.getComps("gspec.xml") 
+    for key in dict.keys():
+        print key, dict[key]
---
+++ GCGF/IDEGenerator/IDEGenerator.py
@@ -0,0 +1,140 @@
+__author__ = "Michael Gonzalez"
+__email__ = "mmg005@eng.ucsd.edu"
+
+import json
+import unicodedata
+import xml.etree.ElementTree as ET
+from GspecParser import GspecParser
+import os
+import copy
+from StringIO import StringIO
+import jinja2
+
+
+
+
+class IDEGenerator:
+    # Default constructor
+    def __init__(self):
+        self.components = None
+        self.JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+        self.jinja_vars = {"blocklist":[]}
+        
+    def setJinjaTemplate(self, template):
+        self.template = self.JINJA_ENVIRONMENT.get_template(template)
+        
+    # Loads default blocks xml to build the Blockly toolbox
+    def loadDefaultBlocks( self, blocksXml ):
+        self.categoriesXML = ET.Element("xml")
+        self.categoriesXML.attrib["id"] = "toolbox"
+        self.categoriesXML.attrib["style"] = "display: none"
+        # Grab the default categories
+        default_block_root = ET.parse(blocksXml)
+	if type(blocksXml) is unicode: 
+	    default_block_root = ET.fromstring(blocksXml)
+        #Append each block to our template
+        for block in default_block_root.getroot():
+            self.categoriesXML.append( block )
+    # Loads the block json into the object
+    def loadBlockDefinitions( self, blockJson ):
+        # Type checking
+        if type (blockJson) == str:
+            stream = None
+            # Is it a file?
+            if os.path.isfile( blockJson):
+                stream = open(blockJson)
+            # Otherwise, treat it like a string
+            else:
+                stream = StringIO( blockJson )
+            self.blocks = json.load(stream)
+        # For some reason, it seems like json objects get passed as lists
+        elif type (blockJson) == list:
+            self.blocks = blockJson
+        else:
+            print "Unknown type: ", type(blockJson)
+            raise "Unknown type passed to loadBlockDefinitions"
+
+    # Loads the gspec into the object using Priyanka's gspec parser
+    # Saves the gspec, doesn't return anything
+    def loadGspec( self, gspec, catalog ):
+        gspecParser = GspecParser()
+        gspecParser.setCatalog(catalog)
+        self.components = gspecParser.getComps(gspec)
+        
+    def createBlockSubset(self):
+        self.blockCategories = {}
+        totalNumberOfCategories = 0
+        componentIdx = 1.0
+        for k in self.components.keys():
+            totalNumberOfCategories = totalNumberOfCategories + self.components[k]
+        for component in self.components.keys():
+            for i in range( 1, self.components[component] + 1 ):
+                # Some useful aliases
+                name = component + " " + str(i)
+                
+                # We want a new colour for this subset
+                color = str(int((componentIdx / totalNumberOfCategories) * 360))
+                componentIdx = componentIdx + 1
+                
+                # The blocks for just this component will be called localBlocks
+                localBlocks = {}
+                
+                # Create the categoriesXML node
+                categoryNode = ET.SubElement(self.categoriesXML, "category" )
+                categoryNode.attrib["name"] = name
+                categoryNode.attrib["colour"] = color
+                
+                # Iterate over each block in our copy so we can make a unique instance
+                jsonElem = copy.deepcopy(self.blocks[component])
+                for block in jsonElem:
+                    block["id"] = "_" + component.lower() + str(i) + "_" + block["id"]
+                    id = block["id"].encode('ascii', 'ignore')
+                    block["colour"] = color
+                    # Add the new block to its proper category
+                    blockNode = ET.SubElement(categoryNode, "block")
+                    blockNode.attrib["type"] = id
+                    blockNode.text = " "
+                    localBlocks[ block["id"] ] = block
+                    self.jinja_vars["blocklist"].append( [ id, str(json.dumps(block)) ] )
+                self.blockCategories[ name ] = localBlocks
+        self.jinja_vars["toolbox"] = str(ET.tostring( self.categoriesXML ))
+	#print self.jinja_vars["toolbox"]
+    
+    def renderIDE(self):
+        return self.template.render(self.jinja_vars)
+                
+if __name__ == "__main__":
+
+    # Default file locations
+    jsonFile = "Blocks.json"
+    catalog = "Components.cat"
+    jinjaFile = "index.jinja"
+    blocksXml = "DefaultCategories.xml"
+    # Arg parse stuff
+    import argparse
+    parser = argparse.ArgumentParser(description="IDEGenerator.py creates a Blockly IDE for Gadgetron. It uses Jinja to create the IDE")
+    parser.add_argument("-j", "--json", required=False)
+    parser.add_argument("-c", "--catalog", required=False)
+    parser.add_argument("-g", "--gspec", required=True)
+    parser.add_argument("-d", "--default_blocks", required=False)
+    parser.add_argument("-x", "--jinja", required=False)
+    args = parser.parse_args()
+    if args.jinja is not None:
+        jinjaFile = args.jinja
+    if args.json is not None:
+        jsonFile = args.json
+    if args.default_blocks is not None:
+        blocksXml = args.default_blocks
+    if args.catalog is not None:
+        catalog = args.catalog
+
+    generator = IDEGenerator()
+    generator.setJinjaTemplate( jinjaFile )
+    generator.loadBlockDefinitions( jsonFile )
+    generator.loadDefaultBlocks( blocksXml )
+    generator.loadGspec( args.gspec, catalog )
+    generator.createBlockSubset()
+    #for key in generator.blockCategories.keys():
+    #    print key, json.dumps(generator.blockCategories[key], indent=4)
+    #print ET.tostring(generator.categoriesXML )
+    print generator.renderIDE().encode('ascii','ignore')
