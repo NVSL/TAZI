024e5a02b3da24061b49ee65fc6f5204b48a96d4
Priyanka Kulshreshtha <pinkikul@yahoo.com>
2016-02-24 18:43:36 -0800

    Script to run all tests

--- attemptsatxmlparsing/runTests.py
+++ attemptsatxmlparsing/runTests.py
@@ -19,6 +19,27 @@ class TestBlocklyTranslator(unittest.TestCase):
 				print("Testing " + xmlFile + "...")
 				output = os.popen("python blocklyTranslator.py -x tests/xml/" + xmlFile).read()
 				print(output)
+		varFiles = os.popen("ls tests/varTests/").read().split("\n")
+		for varFile in varFiles:
+			if re.match(r".*\.xml", varFile):
+				print("Testing " + varFile + "...")
+				output = os.popen("python blocklyTranslator.py -x tests/varTests/" + varFile).read()
+				print(output)
+		mathFiles = os.popen("ls tests/MathTests/").read().split("\n")
+		for mathFile in mathFiles:
+			if re.match(r".*\.xml", mathFile):
+				print("Testing " + mathFile + "...")
+				output = os.popen("python blocklyTranslator.py -x tests/mathTests/" + mathFile).read()
+				print(output)
+		LogicFiles = os.popen("ls tests/LogicTests/").read().split("\n")
+		for logicFile in LogicFiles:
+			if re.match(r".*\.xml", logicFile):
+				print("Testing " + logicFile + "...")
+				output = os.popen("python blocklyTranslator.py -x tests/logicTests/" + logicFile).read()
+				print(output)
+
+
+
 
 
 if __name__ == "__main__":
