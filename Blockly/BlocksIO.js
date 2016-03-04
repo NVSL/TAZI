aedab07eddfe3d3e7c0c0a523d13295765b7ac1e
Michael Gonzalez <mmg005@ucsd.edu>
2016-03-03 23:08:53 -0800

    front end updates

--- Blockly/BlocksIO.js
+++ Blockly/BlocksIO.js
@@ -1,3 +1,37 @@
+window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+
+//window.requestFileSystem(type, size, successCallback, opt_errorCallback)
+
+
+
+function errorHandler(e) {
+  var msg = '';
+
+  switch (e.code) {
+    case FileError.QUOTA_EXCEEDED_ERR:
+      msg = 'QUOTA_EXCEEDED_ERR';
+      break;
+    case FileError.NOT_FOUND_ERR:
+      msg = 'NOT_FOUND_ERR';
+      break;
+    case FileError.SECURITY_ERR:
+      msg = 'SECURITY_ERR';
+      break;
+    case FileError.INVALID_MODIFICATION_ERR:
+      msg = 'INVALID_MODIFICATION_ERR';
+      break;
+    case FileError.INVALID_STATE_ERR:
+      msg = 'INVALID_STATE_ERR';
+      break;
+    default:
+      msg = 'Unknown Error';
+      break;
+  };
+
+  console.log('Error: ' + msg);
+}
+
+
 function loadXML( xmlstr ) {
     var xml = Blockly.Xml.textToDom(xmlstr); 
     var workspace = Blockly.getMainWorkspace();
@@ -27,5 +61,34 @@ function readXMLFromFile(evt) {
     else { alert("Failed to load file"); }
 }
 
+function writeXMLToFile(fs) {
+    fs.root.getFile('blockly.xml', {create: true}, function(fileEntry) {
+    console.log("hi");
+
+    // Create a FileWriter object for our FileEntry (log.txt).
+    fileEntry.createWriter(function(fileWriter) {
+
+      fileWriter.onwriteend = function(e) {
+        console.log('Write completed.');
+      };
+
+      fileWriter.onerror = function(e) {
+        console.log('Write failed: ' + e.toString());
+      };
+
+      // Create a new Blob and write it to log.txt.
+      var blob = new Blob(['Lorem Ipsum'], {type: 'text/plain'});
+
+      fileWriter.write(blob);
+
+    }, errorHandler);
+
+  }, errorHandler);
+
+}
+
+window.requestFileSystem(window.TEMPORARY, 1024*1024, writeXMLToFile, errorHandler);
+
 document.getElementById('xmlinput').addEventListener('change', readXMLFromFile, false);
+document.getElementById('savexml').addEventListener('change', writeXMLToFile, false);
 
