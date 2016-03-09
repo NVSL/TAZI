b472c552706aa437c340819db6d0eb8df7586c59
ersp-admin <ersp-admin@ersp1.com>
2016-03-09 00:19:15 -0500

    Decoupled static file handlers from main.py

--- Blockly/appengine/main.py
+++ Blockly/appengine/main.py
@@ -1,10 +1,8 @@
 import wsgiref.simple_server
 import webapp2
-import os
-from lxml import etree as ET
+import StaticHandler
 
 INDEX = "static/index.html"
-STATIC = "static/"
 
 # Real Request Handlers
 class IDERequestHandler(webapp2.RequestHandler):
@@ -24,26 +22,6 @@ class AspTestHandler(webapp2.RequestHandler):
         print "I got an asp test request!"
 	print request
 
-# Auto Generated Static Request Handlers
-def openStaticFile( fn ): return open(STATIC+fn).read()
-
-def createHandler( static_file ):
-    class Handler(webapp2.RequestHandler):
-        def get(self):
-	    static_str = openStaticFile( static_file )
-	    self.response.write( static_str )
-    return Handler
-
-def create_path_pair( static_file ): 
-    return ( "/" + static_file, createHandler( static_file ) )
-
-def parseStaticFiles( xml_file ):
-    root = ET.parse(xml_file).getroot()
-    static_dir = root.attrib["dir"]
-    files = [f.attrib["path"] for f in root ]
-    return (static_dir, files)
-    
-
 def main (app):
     port = 8080
     httpd = wsgiref.simple_server.make_server('', port, app)
@@ -55,10 +33,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="From a gspec and a component library, compiles a schematic and semi-placed board.")
     parser.add_argument("-s", "--static_files", type=str, nargs="*", default="static_files.xml", help="the complete list of static files that will be served by the server")
     args = parser.parse_args()
-    STATIC, files = parseStaticFiles( args.static_files )
+    STATIC, files = StaticHandler.parseStaticFiles( args.static_files )
     app = webapp2.WSGIApplication([ 
         ("/", IDERequestHandler),
 	("/swag", TestRequestHandler),
 	("/demo_test.asp", AspTestHandler),
-	] + [ create_path_pair(f) for f in files ]  , debug=True)
+	] + [ StaticHandler.create_path_pair(f) for f in files ]  , debug=True)
     main(app)
