20eec8db45aa6fa31e2daee55bfd5701111e6fc7
Michael Gonzalez <mmg005@ucsd.edu>
2016-03-20 13:12:54 -0700

    added compiling dialog

--- Server/controllers/IDEController.js
+++ Server/controllers/IDEController.js
@@ -1,14 +1,34 @@
 var tazi = angular.module('Tazi', ['ngMaterial']);
+function CompilingDialogController($scope, $mdDialog) {
+    var xml = getXML();
+    $.post("/compile",  { "xml" : xml } , function(data) {
+        console.log(data);
+        $mdDialog.hide();
+          //alert(data);
+    });
+}
 tazi.controller('AppCtrl', function($scope, $mdDialog, $mdMedia) {
   $scope.status = '  ';
   $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
   $scope.createNewProgram = function() { 
       console.log($scope.project.programName); 
   };
-  // Post Requests
-  $scope.sendCompilePost = function() { 
-      testPost();
+  $scope.sendCompilePost = function(ev) { 
+    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+    $mdDialog.show({
+      controller: CompilingDialogController,
+      templateUrl: '/static/views/CompilingDialog.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      fullscreen: useFullScreen
+    });
   };
+  $scope.$watch(function() {
+    return $mdMedia('xs') || $mdMedia('sm');
+  }, function(wantsFullScreen) {
+    $scope.customFullscreen = (wantsFullScreen === true);
+  });
+  // Post Requests
   $scope.sendKillPost = function() {
       sendKillSignal();
   };
--- Server/lib/blockly/requests.js
+++ Server/lib/blockly/requests.js
@@ -1,11 +1,3 @@
-function testPost() {
-    var xml = getXML();
-    $.post("/compile",  { "xml" : xml } , function(data) {
-        console.log(data);
-        alert(data);
-    });
-}
-
 function sendKillSignal() {
     $.post("/killprogram" ); 
 }
