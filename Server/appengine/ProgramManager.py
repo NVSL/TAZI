8ecc3e703863a3e9a70ba8d022c2f7cf733e7896
Michael Gonzalez <mmg005@ucsd.edu>
2016-03-31 09:14:19 -0700

    Caught an error with multiple compilations

--- Server/appengine/ProgramManager.py
+++ Server/appengine/ProgramManager.py
@@ -5,6 +5,7 @@ running = "Running"
 not_running = "Not running"
 not_loaded = "No program loaded"
 class ProgramManager:
+    process = None
     def __init__( self, name=not_loaded, program=None ):
         self.name = name
 	self.program = program 
@@ -16,14 +17,14 @@ class ProgramManager:
         try: self.kill()
 	except: pass
 	if self.name == not_loaded: return
-	self.proc = subprocess.Popen("exec " + self.program, **popen_args)
+	ProgramManager.process = subprocess.Popen("exec " + self.program, **popen_args)
 	self.status = running
 	#print self.proc.__dict__
 
     @ensureProgramExists("kill")
     @ensureProcessExists("kill")
     def kill( self ):
-        self.proc.kill()
+        ProgramManager.process.kill()
 	self.status = not_running
 
     @ensureProgramExists("read from")
